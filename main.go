7266e9b8585eef924246cf31b1e946f4f972ba26
JNickBaughan <JNickBaughan@gmail.com>
2021-01-21 20:06:08 -0500

    use wait group

--- main.go
+++ main.go
@@ -3,16 +3,34 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"sync"
 )
 
 func main() {
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(2)
+
+	go func() {
+		
+		count("hello", 50)
+		waitGroup.Done()
+	}()
+
+	go func() {
+
+		count("world", 50)
+		waitGroup.Done()
+	}()
+
+	waitGroup.Wait()
+
 	// two go routines won't do anything. The main go routine will finish before they can be run
-	go count("hello", 50)
-	go count("world",50)
+	// go count("hello", 50)
+	// go count("world",50)
 	// sleeping the program can fix it
 	//time.Sleep(time.Second * 5)
 	// so can this - it will wait for user input to stop the program
-	fmt.Scanln()
+	//fmt.Scanln()
 }
 
 func count(printMe string, times int){
